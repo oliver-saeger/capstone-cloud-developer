e307091ac9dbb30482b79954fd548833c4593c17
Oliver Saeger <saeger.oliver@gmail.com>
2022-08-06 12:58:32 +0200

    configure api

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'jn3peg5x10'
+const apiId = 'e0ehph3xrd'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-kko1f-vc.us.auth0.com',            // Auth0 domain
-  clientId: 'Q71J1DjHzkf982JNy56AM4cRicJrlbSJ',          // Auth0 client id
+  clientId: 'xv9wQ06XknemeApDEKec45Tomupm3QYE',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
